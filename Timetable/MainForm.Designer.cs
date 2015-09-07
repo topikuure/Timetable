b9c78a984f997cf2e9cff253a9bfc08480f204f2
topikuure <topikuure@users.noreply.github.com>
2015-09-07 18:12:04 +0300

    Lisättiin Visual Studio -projekti.
    Projekti sisältää ohjelman päänäkymän sekä menupalkin, mutta ei mitään
    toiminnallisuuksia.

---
+++ Timetable/MainForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Timetable
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timetableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.mondayLabel = new System.Windows.Forms.Label();
+            this.tuesdayLabel = new System.Windows.Forms.Label();
+            this.wednesdayLabel = new System.Windows.Forms.Label();
+            this.thursdayLabel = new System.Windows.Forms.Label();
+            this.fridayLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uusiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tallennaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tulostaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.poistuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.courseManagerPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.addCourseButton = new System.Windows.Forms.Button();
+            this.courseListPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.timetableLayoutPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.courseManagerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timetableLayoutPanel
+            // 
+            this.timetableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.timetableLayoutPanel.ColumnCount = 6;
+            this.timetableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.timetableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.timetableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.timetableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.timetableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.timetableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.timetableLayoutPanel.Controls.Add(this.mondayLabel, 1, 0);
+            this.timetableLayoutPanel.Controls.Add(this.tuesdayLabel, 2, 0);
+            this.timetableLayoutPanel.Controls.Add(this.wednesdayLabel, 3, 0);
+            this.timetableLayoutPanel.Controls.Add(this.thursdayLabel, 4, 0);
+            this.timetableLayoutPanel.Controls.Add(this.fridayLabel, 5, 0);
+            this.timetableLayoutPanel.Controls.Add(this.label1, 0, 1);
+            this.timetableLayoutPanel.Controls.Add(this.label2, 0, 2);
+            this.timetableLayoutPanel.Controls.Add(this.label3, 0, 3);
+            this.timetableLayoutPanel.Controls.Add(this.label4, 0, 4);
+            this.timetableLayoutPanel.Controls.Add(this.label5, 0, 5);
+            this.timetableLayoutPanel.Controls.Add(this.label6, 0, 6);
+            this.timetableLayoutPanel.Controls.Add(this.label7, 0, 7);
+            this.timetableLayoutPanel.Controls.Add(this.label8, 0, 8);
+            this.timetableLayoutPanel.Location = new System.Drawing.Point(204, 39);
+            this.timetableLayoutPanel.Name = "timetableLayoutPanel";
+            this.timetableLayoutPanel.RowCount = 9;
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.timetableLayoutPanel.Size = new System.Drawing.Size(523, 392);
+            this.timetableLayoutPanel.TabIndex = 0;
+            // 
+            // mondayLabel
+            // 
+            this.mondayLabel.AutoSize = true;
+            this.mondayLabel.Location = new System.Drawing.Point(91, 1);
+            this.mondayLabel.Name = "mondayLabel";
+            this.mondayLabel.Size = new System.Drawing.Size(23, 13);
+            this.mondayLabel.TabIndex = 0;
+            this.mondayLabel.Text = "MA";
+            // 
+            // tuesdayLabel
+            // 
+            this.tuesdayLabel.AutoSize = true;
+            this.tuesdayLabel.Location = new System.Drawing.Point(178, 1);
+            this.tuesdayLabel.Name = "tuesdayLabel";
+            this.tuesdayLabel.Size = new System.Drawing.Size(17, 13);
+            this.tuesdayLabel.TabIndex = 1;
+            this.tuesdayLabel.Text = "TI";
+            // 
+            // wednesdayLabel
+            // 
+            this.wednesdayLabel.AutoSize = true;
+            this.wednesdayLabel.Location = new System.Drawing.Point(265, 1);
+            this.wednesdayLabel.Name = "wednesdayLabel";
+            this.wednesdayLabel.Size = new System.Drawing.Size(21, 13);
+            this.wednesdayLabel.TabIndex = 2;
+            this.wednesdayLabel.Text = "KE";
+            // 
+            // thursdayLabel
+            // 
+            this.thursdayLabel.AutoSize = true;
+            this.thursdayLabel.Location = new System.Drawing.Point(352, 1);
+            this.thursdayLabel.Name = "thursdayLabel";
+            this.thursdayLabel.Size = new System.Drawing.Size(22, 13);
+            this.thursdayLabel.TabIndex = 3;
+            this.thursdayLabel.Text = "TO";
+            // 
+            // fridayLabel
+            // 
+            this.fridayLabel.AutoSize = true;
+            this.fridayLabel.Location = new System.Drawing.Point(439, 1);
+            this.fridayLabel.Name = "fridayLabel";
+            this.fridayLabel.Size = new System.Drawing.Size(21, 13);
+            this.fridayLabel.TabIndex = 4;
+            this.fridayLabel.Text = "PE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "8:15";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "9:15";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "10:15";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "11:15";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "12:15";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "13:15";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "14:15";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 345);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "15:15";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(739, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uusiToolStripMenuItem,
+            this.avaaToolStripMenuItem,
+            this.tallennaToolStripMenuItem,
+            this.tulostaToolStripMenuItem,
+            this.poistuToolStripMenuItem});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // uusiToolStripMenuItem
+            // 
+            this.uusiToolStripMenuItem.Name = "uusiToolStripMenuItem";
+            this.uusiToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.uusiToolStripMenuItem.Text = "Uusi";
+            // 
+            // avaaToolStripMenuItem
+            // 
+            this.avaaToolStripMenuItem.Name = "avaaToolStripMenuItem";
+            this.avaaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.avaaToolStripMenuItem.Text = "Avaa";
+            // 
+            // tallennaToolStripMenuItem
+            // 
+            this.tallennaToolStripMenuItem.Name = "tallennaToolStripMenuItem";
+            this.tallennaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tallennaToolStripMenuItem.Text = "Tallenna";
+            // 
+            // tulostaToolStripMenuItem
+            // 
+            this.tulostaToolStripMenuItem.Name = "tulostaToolStripMenuItem";
+            this.tulostaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tulostaToolStripMenuItem.Text = "Tulosta";
+            // 
+            // poistuToolStripMenuItem
+            // 
+            this.poistuToolStripMenuItem.Name = "poistuToolStripMenuItem";
+            this.poistuToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.poistuToolStripMenuItem.Text = "Poistu";
+            // 
+            // courseManagerPanel
+            // 
+            this.courseManagerPanel.ColumnCount = 1;
+            this.courseManagerPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.courseManagerPanel.Controls.Add(this.addCourseButton, 0, 0);
+            this.courseManagerPanel.Controls.Add(this.courseListPanel, 0, 1);
+            this.courseManagerPanel.Location = new System.Drawing.Point(0, 40);
+            this.courseManagerPanel.Name = "courseManagerPanel";
+            this.courseManagerPanel.RowCount = 2;
+            this.courseManagerPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.courseManagerPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.courseManagerPanel.Size = new System.Drawing.Size(200, 391);
+            this.courseManagerPanel.TabIndex = 2;
+            // 
+            // addCourseButton
+            // 
+            this.addCourseButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addCourseButton.Location = new System.Drawing.Point(3, 3);
+            this.addCourseButton.Name = "addCourseButton";
+            this.addCourseButton.Size = new System.Drawing.Size(194, 33);
+            this.addCourseButton.TabIndex = 0;
+            this.addCourseButton.Text = "Lisää kurssi";
+            this.addCourseButton.UseVisualStyleBackColor = true;
+            // 
+            // courseListPanel
+            // 
+            this.courseListPanel.AutoScroll = true;
+            this.courseListPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.courseListPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.courseListPanel.Location = new System.Drawing.Point(3, 42);
+            this.courseListPanel.Name = "courseListPanel";
+            this.courseListPanel.Size = new System.Drawing.Size(194, 346);
+            this.courseListPanel.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 443);
+            this.Controls.Add(this.courseManagerPanel);
+            this.Controls.Add(this.timetableLayoutPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Lukujärjestys";
+            this.timetableLayoutPanel.ResumeLayout(false);
+            this.timetableLayoutPanel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.courseManagerPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel timetableLayoutPanel;
+        private System.Windows.Forms.Label mondayLabel;
+        private System.Windows.Forms.Label tuesdayLabel;
+        private System.Windows.Forms.Label wednesdayLabel;
+        private System.Windows.Forms.Label thursdayLabel;
+        private System.Windows.Forms.Label fridayLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tiedostoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uusiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avaaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tallennaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tulostaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem poistuToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel courseManagerPanel;
+        private System.Windows.Forms.Button addCourseButton;
+        private System.Windows.Forms.FlowLayoutPanel courseListPanel;
+    }
+}
+
