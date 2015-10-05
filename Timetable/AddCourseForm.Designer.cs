6834918997057f5002e150114db8f4e0c2941979
topikuure <topikuure@users.noreply.github.com>
2015-10-05 16:45:12 +0300

    Yksinkertaistettiin AddCourseFormia

--- Timetable/AddCourseForm.Designer.cs
+++ Timetable/AddCourseForm.Designer.cs
@@ -28,64 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.nameLabel = new System.Windows.Forms.Label();
-            this.nameColorButton = new System.Windows.Forms.Button();
+            this.courseNameLabel = new System.Windows.Forms.Label();
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.saveButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // courseNameLabel
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.Controls.Add(this.nameLabel, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.nameColorButton, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.nameTextBox, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 27);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // nameLabel
-            // 
-            this.nameLabel.AutoSize = true;
-            this.nameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.nameLabel.Location = new System.Drawing.Point(3, 0);
-            this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(79, 27);
-            this.nameLabel.TabIndex = 0;
-            this.nameLabel.Text = "Nimi";
-            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // nameColorButton
-            // 
-            this.nameColorButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.nameColorButton.Location = new System.Drawing.Point(173, 3);
-            this.nameColorButton.Name = "nameColorButton";
-            this.nameColorButton.Size = new System.Drawing.Size(24, 21);
-            this.nameColorButton.TabIndex = 3;
-            this.nameColorButton.UseVisualStyleBackColor = true;
+            this.courseNameLabel.AutoSize = true;
+            this.courseNameLabel.Location = new System.Drawing.Point(67, 22);
+            this.courseNameLabel.Name = "courseNameLabel";
+            this.courseNameLabel.Size = new System.Drawing.Size(62, 13);
+            this.courseNameLabel.TabIndex = 0;
+            this.courseNameLabel.Text = "Kurssin nimi";
+            this.courseNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(88, 3);
+            this.nameTextBox.Location = new System.Drawing.Point(135, 19);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(79, 20);
+            this.nameTextBox.Size = new System.Drawing.Size(91, 20);
             this.nameTextBox.TabIndex = 6;
             // 
             // saveButton
             // 
             this.saveButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.saveButton.Location = new System.Drawing.Point(137, 45);
+            this.saveButton.Location = new System.Drawing.Point(151, 51);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 1;
@@ -96,7 +65,7 @@
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(12, 45);
+            this.cancelButton.Location = new System.Drawing.Point(54, 51);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 2;
@@ -107,25 +76,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(230, 90);
+            this.ClientSize = new System.Drawing.Size(280, 86);
+            this.Controls.Add(this.courseNameLabel);
             this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "AddCourseForm";
             this.Text = "Lisää Kurssi";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label courseNameLabel;
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.Button nameColorButton;
         private System.Windows.Forms.TextBox nameTextBox;
     }
 }
